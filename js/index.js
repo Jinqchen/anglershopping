ed446f0cd358c1d92a39fb480ab8ad111cd83e2f
jinquan chen <jinqchen@cs.stonybrook.edu>
2023-05-25 23:53:07 -0400

    change item when click different tab, unfished,

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
 var list = document.getElementById("list");
 var li = list.getElementsByTagName("li");
 
+var pic =document.getElementById("pic");
+var ul = pic.getElementsByTagName("ul");
+
 for(var i = 0; i<li.length;i++){
     li[i].onclick = gotocatalog;
 }
 
 function gotocatalog(){
     for (var i=0;i<li.length;i++){
+        
         if(li[i]===this){
             li[i].className="active";
+            ul[i].className="clearfix active";
         }else{
             li[i].className="nav-item";
+            ul[i].className="clearfix";
         }
+       
     }
 }
