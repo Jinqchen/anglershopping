5a0ae44af7e9c42bc23daa6b82c321ea64625f89
jinquan chen <jinqchen@cs.stonybrook.edu>
2023-05-29 22:40:08 -0400

    finish item list and catalog

--- js/index.js
+++ js/index.js
@@ -3,21 +3,43 @@ var li = list.getElementsByTagName("li");
 
 var pic =document.getElementById("pic");
 var ul = pic.getElementsByTagName("ul");
+var all = document.getElementById("all");
 
 for(var i = 0; i<li.length;i++){
     li[i].onclick = gotocatalog;
 }
 
 function gotocatalog(){
+    console.log(ul);
     for (var i=0;i<li.length;i++){
-        
+        console.log(this.id);
         if(li[i]===this){
             li[i].className="active";
-            ul[i].className="clearfix active";
+            if(i!=0){
+                ul[i-1].className="active"
+            }else{
+                ul[i].className="active";
+            }
+            
         }else{
+            //ul[i].className="other";
             li[i].className="nav-item";
-            ul[i].className="clearfix";
-        }
+            if(i!=0){
+                ul[i-1].className="other";
+                ul[li.length-1].className="other";
+            }else{
+                ul[i].className="other";
+            }
+            
+        }  
        
+    }
+    console.log(all===this);
+
+    if(all===this){
+        console.log("go");
+        for (var i=0;i<li.length;i++){
+            ul[i].className="active";
+        }
     }
 }
